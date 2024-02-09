82b204076bfd4947930ed54cba90b6d0f1b8a46a
Marcos Dias <62249961+203marcos@users.noreply.github.com>
2024-02-08 22:18:54 -0300

    Update Func.java

--- PRIMEIRO/Func.java
+++ PRIMEIRO/Func.java
@@ -66,6 +66,14 @@ public class Func {
 
     }
 
+    public int fib(int n) {
+        if (n == 1)
+            return 0;
+        else if (n == 2)
+            return 1;
+        else
+            return fib(n-2) + fib(n-1);
+    }
 
 
 
